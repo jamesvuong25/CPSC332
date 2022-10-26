ac4c392b670b9ad7f8e3ba92d48dee81cc15b27a
jamesvuong25 <jvuong@zagmail.gonzaga.edu>
2022-10-25 19:18:39 -0700

    Done

--- docs/Participation/10-21/myscript.js
+++ docs/Participation/10-21/myscript.js
@@ -8,11 +8,28 @@ window.onload = function() {
 }
 
 
-function checkForm() {
+function checkSubmission() {
     if (label1.value == "" || label2.value == "" || label3.value == "") 
     {
         window.alert("Please fill out AND save all entries before submitting!");
     }
 }
 
+function saveData() {
+    sessionStorage.setItem("label1", label1.value);
+    sessionStorage.setItem("label2", label2.value);
+    sessionStorage.setItem("label3", label3.value);
+    
+    //changing button text to let user know data has been saved
+    saveButton1.innerText = "Data Saved";
+    saveButton2.innerText = "Data Saved";
+    saveButton3.innerText = "Data Saved";
+}
+
+function checkSaveData() {
+    if (label1.value == "" || label2.value == "" || label3.value == "") 
+    {
+        window.alert("2 Please fill out AND save all entries before submitting!");
+    }
+}
 
